aa9408d702688ecadbbdda4fe515ec03d0c29e16
chris <christopher102994@gmail.com>
2020-01-01 22:44:50 -0500

    Update make_container.sh

--- scripts/make_container.sh
+++ scripts/make_container.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo docker build --no-cache \
 		  --pull \
-    		  --build-arg BUILD_DATE="TODO" \
+    		  --build-arg BUILD_DATE="$(date)" \
 		  --build-arg VERSION="TODO" \
    		  -f Dockerfile \
-		  -t "$DOCKER_REPO"/"$DOCKER_NAME"-"$DOCKER_TAG" \
+		  -t "$DOCKER_REPO"/"$DOCKER_NAME":"$DOCKER_TAG" \
 		  .
+		  
