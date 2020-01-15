ce86b749615488a76c4e5454492c97f167434dc3
chris <christopher102994@gmail.com>
2020-01-14 20:27:07 -0500

    Update push_to_registry.sh
    Updating push tag

--- scripts/push_to_registry.sh
+++ scripts/push_to_registry.sh
@@ -2,3 +2,4 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push "$DOCKER_REPO"/"$DOCKER_NAME":"$DOCKER_TAG"
+docker push "$DOCKER_REPO"/"$DOCKER_NAME":"$TRAVIS_JOB_ID"
