b05e6359d4b37e28e7c309daa8723caebb21a901
chris <christopher102994@gmail.com>
2020-01-15 21:55:26 -0500

    Update make_container.sh
    Adding rev-parse

--- scripts/make_container.sh
+++ scripts/make_container.sh
@@ -3,6 +3,7 @@ sudo docker build --no-cache \
 		  --pull \
     		  --build-arg BUILD_DATE="$(date)" \
 		  --build-arg VERSION="TODO" \
+		  --build-arg VCS_REF=`git rev-parse --short HEAD` \
    		  -f Dockerfile \
 		  -t "$DOCKER_REPO"/"$DOCKER_NAME":"$DOCKER_TAG" \
 		  .
