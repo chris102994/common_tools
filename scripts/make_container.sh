1241f78f50e9af1e25b2ff0211a63e68df0fe7c9
chris102994 <christoper102994@gmail.com>
2020-01-17 20:05:33 -0500

    Updating tools for debian container

--- scripts/make_container.sh
+++ scripts/make_container.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
-sudo docker build --no-cache \
-		  --pull \
-    		  --build-arg BUILD_DATE="$(date)" \
-		  --build-arg VERSION="TODO" \
-		  --build-arg VCS_REF=`git rev-parse --short HEAD` \
-   		  -f Dockerfile \
-		  -t "$DOCKER_REPO"/"$DOCKER_NAME":"$DOCKER_TAG" \
-		  .
-		  
+DATE=$(date)
+VCS_REF=$(git rev-parse --short HEAD)
+
+if [ -f Dockerfile.debian ]; then
+	sudo docker build \
+	  --no-cache \
+	  --pull \
+	  --build-arg BUILD_DATE="$DATE" \
+	  --build-arg VERSION="TODO" \
+	  --build-arg VCS_REF="$VCS_REF" \
+		  -f Dockerfile.debian \
+	  -t "$DOCKER_REPO"/"$DOCKER_NAME":"$DEBIAN_TAG" \
+	  .
+fi
+
+if [ -f Dockerfile.alpine ]; then
+	sudo docker build \
+	  --no-cache \
+	  --pull \
+	  --build-arg BUILD_DATE="$DATE" \
+	  --build-arg VERSION="TODO" \
+	  --build-arg VCS_REF="$VCS_REF" \
+	  -f Dockerfile.alpine \
+	  -t "$DOCKER_REPO"/"$DOCKER_NAME":"$ALPINE_TAG" \
+	  .
+fi
+
+if [ ! -f Dockerfile.debian ] || [ ! -f Dockerfile.alpine ]; then
+	sudo docker build \
+	  --no-cache \
+	  --pull \
+	  --build-arg BUILD_DATE="$DATE" \
+	  --build-arg VERSION="TODO" \
+	  --build-arg VCS_REF="$VCS_REF" \
+		  -f Dockerfile \
+	  -t "$DOCKER_REPO"/"$DOCKER_NAME":"$DOCKER_TAG" \
+	  .
+fi
--- scripts/push_to_registry.sh
+++ scripts/push_to_registry.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push "$DOCKER_REPO"/"$DOCKER_NAME":"$DOCKER_TAG"
+
+
+if [ -f Dockerfile.debian ]; then
+	docker push "$DOCKER_REPO"/"$DOCKER_NAME":"$DEBIAN_TAG"
+fi
+
+if [ -f Dockerfile.alpine ]; then
+	docker push "$DOCKER_REPO"/"$DOCKER_NAME":"$ALPINE_TAG"
+fi
+
+if [ ! -f Dockerfile.debian ] || [ ! -f Dockerfile.alpine ]; then
+	docker push "$DOCKER_REPO"/"$DOCKER_NAME":"$DOCKER_TAG"
+fi
