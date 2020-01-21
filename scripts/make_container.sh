862cb7863c759a4ed4934594186d385780ba891e
chris102994 <christoper102994@gmail.com>
2020-01-21 18:00:19 -0500

    Trying to use a new build cycle

--- scripts/make_container.sh
+++ scripts/make_container.sh
@@ -1,39 +1,17 @@
 #!/bin/bash
+
 DATE=$(date)
 VCS_REF=$(git rev-parse --short HEAD)
 
-if [ -f Dockerfile.debian ]; then
-	sudo docker build \
-	  --no-cache \
-	  --pull \
-	  --build-arg BUILD_DATE="$DATE" \
-	  --build-arg VERSION="TODO" \
-	  --build-arg VCS_REF="$VCS_REF" \
-		  -f Dockerfile.debian \
-	  -t "$DOCKER_REPO"/"$DOCKER_NAME":"$DEBIAN_TAG" \
-	  .
-fi
 
-if [ -f Dockerfile.alpine ]; then
-	sudo docker build \
-	  --no-cache \
-	  --pull \
-	  --build-arg BUILD_DATE="$DATE" \
-	  --build-arg VERSION="TODO" \
-	  --build-arg VCS_REF="$VCS_REF" \
-	  -f Dockerfile.alpine \
-	  -t "$DOCKER_REPO"/"$DOCKER_NAME":"$ALPINE_TAG" \
-	  .
-fi
 
-if [ ! -f Dockerfile.debian ] || [ ! -f Dockerfile.alpine ]; then
-	sudo docker build \
-	  --no-cache \
-	  --pull \
-	  --build-arg BUILD_DATE="$DATE" \
-	  --build-arg VERSION="TODO" \
-	  --build-arg VCS_REF="$VCS_REF" \
-		  -f Dockerfile \
-	  -t "$DOCKER_REPO"/"$DOCKER_NAME":"$DOCKER_TAG" \
-	  .
-fi
+sudo docker build \
+  --no-cache \
+  --pull \
+  --build-arg BASE_IMAGE="$BASE_IMAGE" \
+  --build-arg BUILD_DATE="$DATE" \
+  --build-arg VERSION="TODO" \
+  --build-arg VCS_REF="$VCS_REF" \
+  -f "$DOCKER_FILE" \
+  -t "$DOCKER_REPO"/"$DOCKER_NAME":"$IMAGE_TAG" \
+  .
--- scripts/push_to_registry.sh
+++ scripts/push_to_registry.sh
@@ -2,15 +2,4 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-
-if [ -f Dockerfile.debian ]; then
-	docker push "$DOCKER_REPO"/"$DOCKER_NAME":"$DEBIAN_TAG"
-fi
-
-if [ -f Dockerfile.alpine ]; then
-	docker push "$DOCKER_REPO"/"$DOCKER_NAME":"$ALPINE_TAG"
-fi
-
-if [ ! -f Dockerfile.debian ] || [ ! -f Dockerfile.alpine ]; then
-	docker push "$DOCKER_REPO"/"$DOCKER_NAME":"$DOCKER_TAG"
-fi
+docker push "$DOCKER_REPO"/"$DOCKER_NAME":"$IMAGE_TAG"
