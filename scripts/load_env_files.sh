49f83cf2c183df8ae4e1272861e95e713c58be7e
chris <christopher102994@gmail.com>
2020-02-28 18:09:21 -0500

    New script to load env files.

---
+++ scripts/load_env_files.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -u # Unset Variables are an error
+set -e # Exit on error
+
+usage() {
+	echo "usage: $(basename "$0") /path/to/env/file [/path/to/env/file...]"
+	exit 1
+}
+
+if [ -z "$1" ]; then
+	usage
+fi
+
+for FILE in "$@"
+do
+	if [ -f "$FILE" ]; then
+		export $(egrep -v '^#' "$FILE" | xargs)
+	else
+		echo "$FILE isn't a file."
+	fi
+done
